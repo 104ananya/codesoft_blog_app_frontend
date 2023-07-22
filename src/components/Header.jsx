524fba5ab27c62a83c0b1378320e6df75382cccc
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-07-22 17:23:37 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { images } from "../constants";
+
+const navItemsInfo = [
+  { name: "Home" },
+  { name: "Articles" },
+  { name: "Pages" },
+  { name: "Pricing" },
+  { name: "FAQs" },
+];
+
+const NavItem = ({name}) => {
+  return (
+    <li>
+      <a href="/">{name}</a>
+    </li>
+  );
+};
+
+const Header = () => {
+  return (
+    <section>
+      <header className="container mx-auto px-10 py-5 flex justify-between items-center">
+        <div>
+          <img src={images.Logo} alt="logo" />
+        </div>
+        <div className="flex gap-x-9 items-center">
+          
+          <ul className="flex gap-x-9 font-semibold">
+            {navItemsInfo.map((item) => (
+              <NavItem key={item.name} name={item.name} />
+            ))}
+          </ul>
+          
+          <button className="border-2 border-forth px-6 py-2 rounded-full text-fourth font-semibold hover:bg-fourth hover:text-first ">
+            Sign in
+          </button>
+        </div>
+      </header>
+    </section>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'first': '#EFE1D1',
+      'second': '#CD6688',
+      'third': '#7A316F',
+      'fourth': '#461959',
+
+    },
     extend: {},
   },
   plugins: [],
-}
+};
 
