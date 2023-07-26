a2bdfd3df08a059f6165f19e910316ac98a0a301
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-07-26 18:09:15 +0530

    article page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 
+import { Routes, Route } from "react-router-dom";
+
 import './App.css';
 import HomePage from './pages/home/HomePage';
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
+
 
 function App() {
   return (
     <div className="App font-opensans">
       
-      <HomePage/>
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage/>} />
+        {/* <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Admin />} />
+          <Route path="comments" element={<Comments />} />
+          <Route path="posts/new" element={<NewPost />} />
+          <Route path="posts/manage" element={<ManagePosts />} />
+          <Route path="posts/manage/edit/:slug" element={<EditPost />} />
+        </Route> */}
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainLayout from '../../components/MainLayout'
+
+
+const ArticleDetailPage = () => {
+  return (
+    <MainLayout>
+        <section>
+            <h1>ArticleDetailPage</h1>
+        </section>
+    </MainLayout>
+    
+  )
+}
+
+export default ArticleDetailPage
