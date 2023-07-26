fcaa673ec83e47a69c2fbdb8d81747ec0d963a02
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-07-26 20:10:28 +0530

    breadcrumb

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import MainLayout from '../../components/MainLayout'
+import BreadCrumbs from './../../components/BreadCrumbs';
 
+const breadCrumbsData = [
+  {name: 'Home', link: '/'},
+  { name: "Blog", link: "/blog" },
+  { name: 'article title', link: `/blog/12` }
+]
 
 const ArticleDetailPage = () => {
   return (
     <MainLayout>
-        <section>
-            <h1>ArticleDetailPage</h1>
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+            <article className="flex-1">
+              <BreadCrumbs data={breadCrumbsData} />
+            </article>
         </section>
     </MainLayout>
     
